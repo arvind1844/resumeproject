ae98cafc1c1383d97a009e72494bc81a8c64488a
Arvind <sarvind0505@gmail.com>
2021-12-15 17:09:28 +0530

    Created static and templates folder inside core
    Added .css and .js files for bootstrap and fontawesome

--- resumeproject/settings.py
+++ resumeproject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core.apps.CoreConfig',
+    'eduexp.apps.EduexpConfig',
+    'skills.apps.SkillsConfig',
 ]
 
 MIDDLEWARE = [
